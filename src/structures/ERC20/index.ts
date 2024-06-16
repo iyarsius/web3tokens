0b798c6087db3d788dad4516f13df6de22c0b44c
iyarsius <iyarsius@gmail.com>
2024-06-17 01:48:08 +0200

    commented ERC20 class

--- src/structures/ERC20/index.ts
+++ src/structures/ERC20/index.ts
@@ -2,28 +2,23 @@ import * as abi from "../../abis/ERC20";
 import { IERC20Events, IERC20ApproveParams, IERC20TransferParams, IERC20TransferFromParams } from "../../types/ERC20";
 import { ContractOperation } from "../../utils/transactions/ContractOperation";
 import { IContractConfig } from "../../types/Contracts";
-import { Address, WatchContractEventReturnType } from "viem";
+import { Address } from "viem";
 
-export interface IERC20 {
-    on<T extends keyof IERC20Events>(eventName: T, callback: IERC20Events[T]): WatchContractEventReturnType;
-    allowance(owner: string, spender: string): Promise<number>;
-    approve: ContractOperation<IERC20ApproveParams>;
-    balanceOf(account: string): Promise<number>;
-    decimals(): Promise<number>;
-    name(): Promise<string>;
-    symbol(): Promise<string>;
-    totalSupply(): Promise<number>;
-    transfer: ContractOperation<IERC20TransferParams>;
-    transferFrom: ContractOperation<IERC20TransferFromParams>;
-}
-
-export class ERC20 implements IERC20 {
+/**
+ * Implementation of ERC20 token standard
+ */
+export class ERC20 {
     address: Address;
 
     constructor(public config: IContractConfig) {
         this.address = config.address
     }
 
+    /**
+     * Listen for contract events
+     * @param eventName the name of the event
+     * @param callback the function to call when the event is triggered
+     */
     on<T extends keyof IERC20Events>(eventName: T, callback: IERC20Events[T]) {
         return this.config.client.public.watchContractEvent({
             address: this.config.address,
@@ -32,6 +27,13 @@ export class ERC20 implements IERC20 {
         })
     };
 
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
     async allowance(owner: string, spender: string): Promise<number> {
         return await this.config.client.public.readContract({
             abi: [abi.allowance],
@@ -41,6 +43,21 @@ export class ERC20 implements IERC20 {
         }) as any
     }
 
+    /**
+     * @dev Sets a `value` amount of tokens as the allowance of `spender` over the
+     * caller's tokens.
+     *
+     * @returns a ContractOperation instance.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * @emits Approval event.
+     */
     public approve = new ContractOperation<IERC20ApproveParams>(this.config.client, {
         abi: [abi.approve],
         address: this.config.address,
@@ -49,6 +66,9 @@ export class ERC20 implements IERC20 {
         chain: this.config.client.wallet.chain!
     });
 
+    /**
+     * @dev Returns the value of tokens owned by `account`.
+     */
     async balanceOf(account: string): Promise<number> {
         return await this.config.client.public.readContract({
             abi: [abi.balanceOf],
@@ -58,6 +78,15 @@ export class ERC20 implements IERC20 {
         }) as any
     }
 
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the default value returned by this function, unless
+     * it's overridden.
+     */
     async decimals(): Promise<number> {
         return await this.config.client.public.readContract({
             abi: [abi.decimals],
@@ -67,6 +96,9 @@ export class ERC20 implements IERC20 {
         }) as any
     }
 
+    /**
+     * @returns the name of the token.
+     */
     async name(): Promise<string> {
         return await this.config.client.public.readContract({
             abi: [abi.name],
@@ -76,6 +108,11 @@ export class ERC20 implements IERC20 {
         }) as any
     }
 
+
+    /**
+     * @returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
     async symbol(): Promise<string> {
         return await this.config.client.public.readContract({
             abi: [abi.symbol],
@@ -85,6 +122,9 @@ export class ERC20 implements IERC20 {
         }) as any
     }
 
+    /**
+     * @returns the value of tokens in existence.
+     */
     async totalSupply(): Promise<number> {
         return await this.config.client.public.readContract({
             abi: [abi.totalSupply],
@@ -94,6 +134,13 @@ export class ERC20 implements IERC20 {
         }) as any
     }
 
+    /**
+     * @dev Moves a `value` amount of tokens from the caller's account to `to`.
+     *
+     * @returns a ContractOperation instance.
+     * 
+     * @emits Transfer event.
+     */
     public transfer = new ContractOperation<IERC20TransferParams>(this.config.client, {
         abi: [abi.transfer],
         address: this.config.address,
@@ -102,6 +149,15 @@ export class ERC20 implements IERC20 {
         chain: this.config.client.wallet.chain!
     });
 
+    /**
+     * @dev Moves a `value` amount of tokens from `from` to `to` using the
+     * allowance mechanism. `value` is then deducted from the caller's
+     * allowance.
+     *
+     * @returns a ContractOperation instance.
+     *  
+     * @emits Transfer event.
+     */
     public transferFrom = new ContractOperation<IERC20TransferFromParams>(this.config.client, {
         abi: [abi.transferFrom],
         address: this.config.address,
--- src/types/ERC20/index.ts
+++ src/types/ERC20/index.ts
@@ -2,34 +2,35 @@ import { Log } from "viem";
 
 export * from "./extensions/ERC20Mintable";
 
-export interface IERC20Events {Approval: (data: (Log & { args: IERC20ApprovalEventParams})) => void;
-Transfer: (data: (Log & { args: IERC20TransferEventParams})) => void;
+export interface IERC20Events {
+  Approval: (data: (Log & { args: IERC20ApprovalEventParams })) => void;
+  Transfer: (data: (Log & { args: IERC20TransferEventParams })) => void;
 };
 export interface IERC20ApprovalEventParams {
-   owner: string;
-spender: string;
-value: number
+  owner: string;
+  spender: string;
+  value: number
 }
 
 export interface IERC20TransferEventParams {
-   from: string;
-to: string;
-value: number
+  from: string;
+  to: string;
+  value: number
 }
 
 export interface IERC20ApproveParams {
   spender: string;
-value: number
- }
+  value: number
+}
 
 export interface IERC20TransferParams {
   to: string;
-value: number
- }
+  value: number
+}
 
 export interface IERC20TransferFromParams {
   from: string;
-to: string;
-value: number
- }
+  to: string;
+  value: number
+}
 
