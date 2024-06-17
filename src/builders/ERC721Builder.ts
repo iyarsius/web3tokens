cc7d08aad2b35ca6068f6192973d8a524b4419c9
iyarsius <iyarsius@gmail.com>
2024-06-17 15:19:09 +0200

    updated ERC721Builder

--- src/builders/ERC721Builder.ts
+++ src/builders/ERC721Builder.ts
@@ -6,11 +6,29 @@ import { Address } from "viem";
 import { ERC721} from "../structures/ERC721";
 import { ERC721Mintable, IERC721Mintable } from "../structures/ERC721/extensions/ERC721Mintable";
 import { IClient } from "../types/Client";
+import { ERC721Burnable, IERC721Burnable } from "../structures/ERC721/extensions/ERC721Burnable";
+import { AccessControl, AccessManaged, IAccessControl, IAccessManaged, IOwnable, Ownable } from "../structures/access";
 
 export class ERC721Builder<T = ERC721> extends ContractBuilder {
   constructor(protected client: IClient) {
     super(ERC721);
   };
+
+  setOwnable(): ERC721Builder<T & IOwnable> {
+    return this.setExtension(Ownable);
+  };
+
+  setAccessControl(): ERC721Builder<T & IAccessControl> {
+    return this.setExtension(AccessControl);
+  };
+
+  setAccessManaged(): ERC721Builder<T & IAccessManaged> {
+    return this.setExtension(AccessManaged);
+  }
+
+  setBurnable(): ERC721Builder<T & IERC721Burnable> {
+    return this.setExtension(ERC721Burnable);
+  }
 
   setMintable(): ERC721Builder<T & IERC721Mintable> {
     return this.setExtension(ERC721Mintable);
